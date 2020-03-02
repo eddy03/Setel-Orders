cd393bc2598fedbcecc746bef68ce595a894d989
Edi Abdul Rahman <eddytech03@gmail.com>
2020-03-02 10:38:33 +0800

    Complete

--- app/models/payments.js
+++ app/models/payments.js
@@ -1,15 +1,18 @@
 const axios = require('axios')
+const jwt = require('jsonwebtoken')
+
+const auth = jwt.sign({id: 'ORDER_SERVICE'}, process.env.ENCRYPTION_KEY)
 
 module.exports = app => {
   const modelMethod = {}
 
   modelMethod.create = data => {
-    return axios.post(`${process.env.PAYMENTS_SERVICE_API}/payments`, data)
+    return axios.post(`${process.env.PAYMENTS_SERVICE_API}/payments`, data, {headers: {Authorization: auth}})
       .then(response => (response.data))
   }
 
   modelMethod.findOne = orderId => {
-    return axios.get(`${process.env.PAYMENTS_SERVICE_API}/payments`, { params: { orderId } })
+    return axios.get(`${process.env.PAYMENTS_SERVICE_API}/payments`, { params: { orderId }, headers: {Authorization: auth} })
       .then(response => (response.data))
   }
 
